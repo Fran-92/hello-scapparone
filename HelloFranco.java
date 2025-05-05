a4451573b65989abcb1bba0608424725a2556e0c
Fran-92 <franco.scapparone@capellinisauro.edu.it>
2025-05-05 07:11:16 +0000

    tre nomi

--- HelloFranco.java
+++ HelloFranco.java
@@ -1,5 +1,7 @@
 class HelloFranco {
 	public static void main(String[] args) {
 		System.out.println("Hello Franco!");
+        System.out.println("Hello Franco!");
+        System.out.println("Hello Franco!");
 	}
 }
